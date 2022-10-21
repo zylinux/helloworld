459f7b5dfd0ecb6efed8c394a4113e345b73bfc1
mjj <mjj@ubuntu20.04.myguest.virtualbox.org>
2022-10-21 17:12:17 -0400

    first

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf ("[master branch]Hello Yocto!\n");//change develop to master
+
+    return 0;
+} /* ----- End of main() ----- */
